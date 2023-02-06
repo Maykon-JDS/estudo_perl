538008cfdf88c9fe3ae66ce8ee99a48d01a29722
Maykon Dias <Maykondias2001@gmail.com>
2023-02-05 22:33:38 -0300

    Update

---
+++ programas/ex_01.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl -n 
+
+if ($ == 1) {
+print "<!DOCTYPE html>\ 
+<html lang=\"en\">\ 
+<head>\ 
+<title>The Rime of the Ancyent Mariner 
+(1798)</title>\ 
+</head>\ 
+<body>\ 
+"; 
+
+s/^/<h1>/; 
+s/$/<\/h1>/m; 
+print; exit; 
+}
+
---
+++ programas/ex_02.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl -n
+
+if($. == 1){ 
+s/^/<h1>/;
+s/$/<\/h1>/m;
+print;exit;
+}
+
+
